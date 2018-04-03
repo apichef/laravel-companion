1dff15e625a80d5af22a770dfc55aa6d5869c863
Milroy Emesh Fraser <milroy@outlook.com>
2018-04-03 16:26:42 +0000

    Apply fixes from StyleCI

--- tests/database/factories/CommentFactory.php
+++ tests/database/factories/CommentFactory.php
@@ -8,10 +8,10 @@ use Sarala\Dummy\User;
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         'body' => $faker->paragraphs(1, true),
-        'user_id' => function() {
+        'user_id' => function () {
             return factory(User::class)->create()->id;
         },
-        'post_id' => function() {
+        'post_id' => function () {
             return factory(Post::class)->create()->id;
         }
     ];
--- tests/database/factories/PostFactory.php
+++ tests/database/factories/PostFactory.php
@@ -10,7 +10,7 @@ $factory->define(Post::class, function (Faker $faker) {
         'subtitle' => $faker->sentences(2, true),
         'body' => $faker->paragraphs(3, true),
         'published_at' => $faker->dateTimeBetween('-1 months', '+1 months'),
-        'author_id' => function() {
+        'author_id' => function () {
             return factory(User::class)->create()->id;
         }
     ];
